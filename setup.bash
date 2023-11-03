a866dc670748509a0e9304bb30a02af86094cc37
ChinYikMing <yikming2222@gmail.com>
2023-11-03 16:44:41 +0800

    add parallel

--- setup.bash
+++ setup.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+
 sudo hciconfig hci0 up
-sudo ./people_counter.py &
-flask --app web/people_counter_server run
+#sudo ./people_counter.py
+#flask --app web/people_counter_server run
+#sudo parallel ::: ./people_counter.py flask --app web/people_counter_server run
+#
+(echo 'sudo ./people_counter.py'; echo 'flask --app web/people_counter_server run') | sudo parallel -j0 --lb
+
+#parallel -j0 --lb  <<EOF
+#echo ./people_counter.py
+#echo flask --app web/people_counter_server run
+#EOF
+
